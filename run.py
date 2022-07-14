089b1273e3fe658d0d928074b4be38f4ac0a5356
ejasquith <emily.asquith@icloud.com>
2022-07-14 01:40:52 +0000

    Update sales worksheet

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def get_sales_data():
         data_str = input(">")
         sales_data = [data.strip() for data in data_str.split(",")]
     
-    return sales_data
+    return [int(data) for data in sales_data]
 
 
 def validate_data(values):
@@ -43,11 +43,22 @@ def validate_data(values):
                 f"6 values expected, {len(values)} provided"
             )
         values = [int(data) for data in values]
-    except ValueError as e:
-        print(f"Invalid data: {e}\nPlease try again.")
+    except ValueError as exception:
+        print(f"Invalid data: {exception}\nPlease try again.")
         return False
     else:
         return True
 
 
+def update_sales_data(data):
+    """
+    Update sales worksheet, add new row with list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated.\n")
+
+
 data = get_sales_data()
+update_sales_data(data)
